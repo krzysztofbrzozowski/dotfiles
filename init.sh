1af2fb584d7cf6c27fcd3ad11009da37e4e42e46
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2025-02-08 00:24:59 +0100

    Init working fine

--- init.sh
+++ init.sh
@@ -2,7 +2,22 @@
 
 CONFIGS_DIR="configs"
 
-find "$CONFIGS_DIR" -type d -exec mkdir -p "$HOME/{}" \;
-find "$CONFIGS_DIR" -type f -exec ln -sf "$PWD/{}" "$HOME/{}" \;
+# Create directories in $HOME, removing "configs/" prefix
+find "$CONFIGS_DIR" -type d -exec sh -c '
+    dir=${1#configs/}  # Remove "configs/" from the path
+    mkdir -p "$HOME/$dir"
+' _ {} \;
+
+# Create symlinks in $HOME, removing "configs/" prefix
+find "$CONFIGS_DIR" -type f -exec sh -c '
+    file=${1#configs/}  # Remove "configs/" from the path
+    ln -sf "$PWD/$1" "$HOME/$file"
+' _ {} \;
+
+
+if [ -d "$HOME/$CONFIGS_DIR" ]; then
+    rm -rf "$HOME/$CONFIGS_DIR"
+fi
 
 echo "Symlinks created successfully."
+
