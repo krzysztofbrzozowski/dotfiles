c958e2604deb426385692590327902aa4e53932d
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2025-02-11 21:55:56 +0100

    Addec clang config

--- configs/.config/nvim/lua/plugins/none-ls.lua
+++ configs/.config/nvim/lua/plugins/none-ls.lua
@@ -1,17 +1,17 @@
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            sources = {
-               null_ls.builtins.formatting.stylua,
-               null_ls.builtins.formatting.black,
-               null_ls.builtins.formatting.isort,
-               null_ls.builtins.diagnostics.erb_lint,
-               null_ls.builtins.diagnostics.erb_lint,
-            },
-        })
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+				null_ls.builtins.diagnostics.erb_lint,
+				null_ls.builtins.diagnostics.erb_lint,
+			},
+		})
 
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
--- configs/.config/nvim/lua/plugins/telescope.lua
+++ configs/.config/nvim/lua/plugins/telescope.lua
@@ -1,34 +1,34 @@
 return {
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup({
-        defaults = {
-	  file_ignore_patterns = { "%.git/" },  -- Optional: Ignore .git folder
-    	  hidden = true,  -- Show hidden files
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
 	},
-	pickers = {
-    	  find_files = {
-	  hidden = true  -- Ensure `find_files` shows hidden files
-    	  }
-  	},
-	extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					file_ignore_patterns = { "%.git/" }, -- Optional: Ignore .git folder
+					hidden = true, -- Show hidden files
+				},
+				pickers = {
+					find_files = {
+						hidden = true, -- Ensure `find_files` shows hidden files
+					},
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
 
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
