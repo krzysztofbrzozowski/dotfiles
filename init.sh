b1ffe9c2eae73036d3862d956de6e73e22fc576d
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2025-02-16 13:43:42 +0100

    Added linux option

--- init.sh
+++ init.sh
@@ -19,5 +19,21 @@ if [ -d "$HOME/$CONFIGS_DIR" ]; then
     rm -rf "$HOME/$CONFIGS_DIR"
 fi
 
+# If system is Linux use leave bashrc, else use .zshrc
+ios_name=$(uname)
+
+if [[ "$ios_name" == "Linux" ]]; then
+    echo "Detected Linux. Removing ~/.zshrc"
+    echo "Detected Linux. Removing ~/.zprofile"
+    rm -f ~/.zshrc
+    rm -f ~/.zshrc
+elif [[ "$ios_name" == "Darwin" ]]; then
+    echo "Detected macOS. Removing ~/.bashrc"
+    rm -f ~/.bashrc
+else
+    echo "Unsupported OS: $ios_name"
+    exit 1
+fi
+
 echo "Symlinks created successfully."
 
